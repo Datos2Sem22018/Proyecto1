36932f2d08532ec46ded68c7f2e3f29596b57697
Mariano-CE <marianomm1301@outlook.com>
2018-09-21 21:05:26 -0600

    Mariano's first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hell World!, Mariano"<< endl;
+    cout << "Hell World!, Mariano version 2.0"
+            ""<< endl;
     return 0;
 }
