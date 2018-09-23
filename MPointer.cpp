507a692f563c4dc69b55355a33050659cda03f48
zuckerberg <prieto.luisdaniel@gmail.com>
2018-09-22 19:13:22 -0600

    Se arreglan los templates

--- MPointer.cpp
+++ MPointer.cpp
@@ -1,23 +1,23 @@
 #include "MPointer.h"
 
 template <class T>
-MPointer::MPointer() {
+MPointer<T>::MPointer() {
 
 }
 
 template <class T>
-MPointer::~MPointer() {
+MPointer<T>::~MPointer() {
 
 }
 
 template <class T>
-MPointer MPointer::New() {
+MPointer<T> MPointer::New() {
     MPointer* myPtr = new MPointer;
     new T;
     return *myPtr;
 }
 
-void MPointer::operator=(MPointer *myPtr) {
+void MPointer::operator=(MPointer<T> *myPtr) {
     if (typeid(myPtr).name() == typeid(this).name()) {
         if (typeid(myPtr->data).name() == typeid(this->data).name()) {
             std::cout << "Operator works" << std::endl;
