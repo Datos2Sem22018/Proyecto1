59bf4b712bf839d928dd09cb61c862f5ce51e703
karlami <karla.rivera24@gmail.com>
2018-09-22 19:24:18 -0600

    Template MPointer

--- MPointer.cpp
+++ MPointer.cpp
@@ -1,23 +1,23 @@
 #include "MPointer.h"
 
 template <class T>
-MPointer::MPointer() {
+MPointer<T>::MPointer() {
 
 }
 
 template <class T>
-MPointer::~MPointer() {
+MPointer<T>::~MPointer() {
 
 }
 
 template <class T>
-MPointer MPointer::New() {
+MPointer<T> MPointer<T>::New() {
     MPointer* myPtr = new MPointer;
     new T;
     return *myPtr;
 }
-
-void MPointer::operator=(MPointer *myPtr) {
+template <class T>
+void MPointer<T>::operator=(MPointer<T> *myPtr) {
     if (typeid(myPtr).name() == typeid(this).name()) {
         if (typeid(myPtr->data).name() == typeid(this->data).name()) {
             std::cout << "Operator works" << std::endl;
@@ -25,4 +25,8 @@ void MPointer::operator=(MPointer *myPtr) {
     } else {
         std::cout << "Doesn't work" << std::endl;
     }
+}
+template <class T>
+T* MPointer<T>::operator&() {
+    return &data;
 }
--- MPointerGC.cpp
+++ MPointerGC.cpp
@@ -1,14 +1,12 @@
 //
 // Created by karla on 22/09/18.
 //
-
 #include "MPointerGC.h"
 MPointerGC* MPointerGC::instance = 0;
 
 MPointerGC* MPointerGC::getInstance()
 {
-    if (instance == 0)
-    {
+    if (instance == 0){
         instance = new MPointerGC();
     }
 
