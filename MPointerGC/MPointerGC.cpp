c0d61088d5787f20e0d6e57e29d04a0f1653996b
Mariano-CE <marianomm1301@outlook.com>
2018-09-24 00:48:53 -0600

    Lunes 0048

--- MPointerGC/MPointerGC.cpp
+++ MPointerGC/MPointerGC.cpp
@@ -1,12 +1,27 @@
 //
 // Created by karla on 22/09/18.
 //
+#include <unistd.h>
 #include "MPointerGC.h"
-MPointerGC* MPointerGC::instance = nullptr;
+MPointerGC* MPointerGC::instance = 0;
 
-MPointerGC* MPointerGC::getInstance() {
-    if (!instance){
+MPointerGC* MPointerGC::getInstance()
+{
+    if (instance == 0){
         instance = new MPointerGC();
     }
+
     return instance;
+}
+
+MPointerGC::MPointerGC()
+{}
+
+
+void MPointerGC::execute() {
+    for(int i=0; i<10; i++){
+        std::cout<<"Execcuting MPointerGC"<<std::endl;
+        usleep(1000000);
+    }
+
 }
